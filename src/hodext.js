56e91afca0cb51b30ed11b63fb9a81c0a566659f
Gokmen Goksel <gokmen@goksel.me>
2016-12-03 19:04:31 +0300

    Hodext: set default options, update debugging

--- src/hodext.js
+++ src/hodext.js
@@ -4,14 +4,17 @@ const debug = _debug('hodext')
 import { app } from 'electron'
 import { HodextController } from './hodextcontroller'
 
-const Controller = new HodextController()
+const Controller = new HodextController({
+  watchImages: false,
+  repeatEvery: 250
+})
 
 app.dock.hide();
 
 app.on('ready', () => {
 
   Controller.on('SaveItem', (item) => {
-    debug('Save item', item.content, 'from app', item.app.name)
+    debug('Save item', item)
   })
 
   debug('APP is ready.')
