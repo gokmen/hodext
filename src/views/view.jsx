13b7485aa27565abb1b907d177056119df2114e9
Gokmen Goksel <gokmen@goksel.me>
2016-12-14 01:59:35 +0300

    Hodext: Views: HodextView initial commit

---
+++ src/views/view.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { focus, getViewController } from './viewcontroller'
+import { HodextSearchInput } from './searchinput'
+import { HodextItem } from './item'
+
+const Controller = getViewController()
+
+export default class HodextView extends React.Component {
+
+  constructor (props, context) {
+    super(props, context)
+    this.state = { items: [] }
+  }
+
+
+  componentDidMount () {
+
+    this.setState({ items: Controller.items })
+
+    Controller.on('ClipboardChanged', () => {
+      this.setState({ items: Controller.items })
+    })
+
+    focus()
+
+  }
+
+
+  render() {
+
+    let items = this.state.items
+      .filter ( (item) => item.visible )
+      .map ( (item, index) =>
+        <HodextItem
+          data={item}
+          key={item.key}
+          active={index == Controller.selectedItem}
+        />
+      )
+
+    return (
+      <div>
+        <HodextSearchInput />
+        <div id='hodext-items'>
+          {items}
+        </div>
+      </div>
+    )
+  }
+
+}
