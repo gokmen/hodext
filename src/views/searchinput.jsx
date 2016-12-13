347cb3362812ad705bac3b250fe55329a731c387
Gokmen Goksel <gokmen@goksel.me>
2016-12-14 01:59:01 +0300

    Hodext: Views: HodextSearchInput initial commit

---
+++ src/views/searchinput.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ipcRenderer } from 'electron'
+import { getViewController } from './viewcontroller'
+
+let Controller = getViewController()
+
+export class HodextSearchInput extends React.Component {
+
+  render() {
+    return (
+      <input
+        id='search-input'
+        type='text'
+        name='search'
+        onChange={this._onChange}
+        onKeyDown={this._onKeyDown}
+      />
+    )
+  }
+
+  _onKeyDown (event) {
+    if (event.keyCode == 40 || (event.keyCode == 74 && event.ctrlKey )) {
+      Controller.activate(1)
+    } else if (event.keyCode == 38 || (event.keyCode == 75 && event.ctrlKey )) {
+      Controller.activate(-1)
+    } else if (event.keyCode == 13) {
+      Controller.useActive(!event.ctrlKey)
+    } else if (event.keyCode == 8 && event.ctrlKey ) {
+      Controller.removeActive()
+    } else if (event.keyCode == 27) {
+      ipcRenderer.send('HIDE_HODEXT')
+    }
+  }
+
+  _onChange (event) {
+    Controller.filter(event.target.value)
+  }
+
+}
