6ec068f7322397e2d800af9a078f6057c364fa23
Gokmen Goksel <gokmen@goksel.me>
2017-01-22 14:41:29 -0800

    Constants: Add constants for Events

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,12 @@
 export const PREFIX = 'hodext'
+export const NEWLINE = '\n'
 export const ITEM_HEIGHT = 34
 export const MAX_CHAR_COUNT = 68
 export const SCROLL_THRESHOLD = 4
 export const MAX_ITEM_COUNT = 500
+
+export const STORAGE_FILE = 'clipboard.json'
+
+export const EVENT_ADDITEM = 'AddItem'
+export const EVENT_SAVEITEM = 'SaveItem'
+export const EVENT_CLIPBOARDCHANGED = 'ClipboardChanged'
